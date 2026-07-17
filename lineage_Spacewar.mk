e64ad7c91adb9442933024752fceec170acf9597
iplaynasty <amansharma1841112016@gmail.com>
2026-07-18 02:49:54 +0530

    Update lineage_Spacewar.mk

--- lineage_Spacewar.mk
+++ lineage_Spacewar.mk
@@ -35,3 +35,40 @@ TARGET_ENABLE_BLUR := true
 
 # Boot Animation
 TARGET_BOOT_ANIMATION_RES := 1080
+
+#axion_flags
+#Camera information (multiple sensors supported)
+AXION_CAMERA_REAR_INFO := 50,50
+AXION_CAMERA_FRONT_INFO := 16
+
+# Maintainer name (underscores become spaces in the UI)
+AXION_MAINTAINER := iplaynasty
+
+# Processor name (underscores become spaces)
+AXION_PROCESSOR := Snapdragon_778G+
+
+TARGET_DISABLE_EPPE := true
+TARGET_INCLUDE_AXFX := true
+TARGET_DISABLES_LIBPERF ?= false
+
+TARGET_NEEDS_DOZE_FIX := false
+TARGET_DOZE_TAP_PULSE_SUPPORTED := true
+TARGET_DOZE_DOUBLE_TAP_PULSE_SUPPORTED := true
+TARGET_DOZE_PICKUP_PULSE_SUPPORTED := true
+TARGET_DOZE_SIDE_FPS_PULSE_SUPPORTED := false
+
+TARGET_OPTIMIZED_DEXOPT := true
+TARGET_SUPPORTED_REFRESH_RATES := 60,90,120
+
+WITH_BCR := true
+TARGET_CUSTOM_UDFPS := true
+
+$(call soong_config_set,surfaceflinger,frame_rate_category_high,120)
+$(call soong_config_set,surfaceflinger,frame_rate_category_min,60)
+
+SURFACE_FLINGER_BOOST := true
+
+PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/axion/certs/releasekey
+
+# 2. Bluetooth mainline module SEPolicy signed with your keys (CRITICAL)
+PRODUCT_MAINLINE_BLUETOOTH_SEPOLICY_DEV_CERTIFICATES := $(dir $(PRODUCT_DEFAULT_DEV_CERTIFICATE))
